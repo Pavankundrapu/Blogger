b1c0aa53e8af1039e982d13419a1ad5355c238bb
Pavan Kumar Kundrapu <iampavanhere@gmail.com>
2024-07-29 20:26:56 +0530

    logo added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Logo({width = '100px'}) {
   return (
-    <div>Logo</div>
+    <img className="h-12 w-auto rounded-xl"  src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572.png" alt="Blogger logo" />
   )
 }
+
 
 export default Logo
