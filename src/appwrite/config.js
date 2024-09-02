fa93d375026b90fc515377a492530cfd6e5c4265
Pavan Kumar Kundrapu <iampavanhere@gmail.com>
2024-09-02 10:15:29 +0530

    this is the commit

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -125,15 +125,10 @@ export class Service{
     }
 
     getFilePreview(fileId){
-       
-       try{
         return this.bucket.getFilePreview(
             conf.appwriteBucketId,
             fileId
         )
-       } catch (error) {
-        console.log("Appwrite serive :: getFilePreview :: error", error);
-        return false
     }
 }
 
